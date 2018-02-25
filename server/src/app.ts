6b9b12dd0063ed994477dfe5e784f44fe292bd52
William Mora <william.r.mora@gmail.com>
2018-02-25 17:48:26 -0500

    Set up basic express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
