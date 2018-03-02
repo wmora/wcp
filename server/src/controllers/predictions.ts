ac1d67a2fa3228c53b563473e5cb7b31ff901c88
William Mora <william.r.mora@gmail.com>
2018-03-01 21:27:56 -0500

    Add more validations

--- server/src/controllers/predictions.ts
+++ server/src/controllers/predictions.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { db } from '../db/db'
+import { MatchStatus } from '../interfaces/match'
 import * as matchController from './matches'
 
 export function postPrediction(request: Request, response: Response): void {
@@ -14,10 +15,14 @@ export function postPrediction(request: Request, response: Response): void {
         return
     }
 
+    if (match.status !== MatchStatus.Pending) {
+        response.status(400).send('Cannot modify prediction for match')
+    }
+
     const isValidWinningTeamId = [match.homeTeamId, match.awayTeamId].some((teamId) => teamId === winningTeamId)
 
     if (!isValidWinningTeamId) {
-        response.sendStatus(400)
+        response.status(400).send(`Invalid winningTeamId: ${winningTeamId}`)
     }
 
     const collection = db.collection('predictions')
