969bd502ce2fd0bb3da1640496ec2bc11da961e0
William Mora <william.r.mora@gmail.com>
2018-03-01 21:44:52 -0500

    List all matches

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import { connect as connectDb } from './db/db'
 import * as bodyParser from 'body-parser'
 import * as express from 'express'
 
+import matchesController = require('./controllers/matches')
 import predictionsController = require('./controllers/predictions')
 
 const app = express()
@@ -11,6 +12,7 @@ connectDb()
 
 app.use(bodyParser.json())
 
+app.get('/matches', matchesController.listMatches)
 app.post('/predictions', predictionsController.postPrediction)
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- server/src/controllers/matches.ts
+++ server/src/controllers/matches.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express'
 import { Match, MatchStatus } from '../interfaces/match'
 import data from '../models/data'
 
@@ -10,16 +11,30 @@ function findMatchInRound(round: any, id: string): Match {
         const match = round[phase].matches.find((m) => `${m.name}` === id)
 
         if (match) {
-            return {
-                id: `${match.name}`,
-                homeTeamId: `${match.home_team}`,
-                awayTeamId: `${match.away_team}`,
-                status: MatchStatus.Pending,
-                date: new Date(match.date),
-                winnerTeamId: ''
-            }
+            return transformMatch(match)
         }
     }
 
     return null
+}
+
+export function listMatches(request: Request, response: Response): void {
+    const matches = []
+
+    for (const group of Object.keys(data.groups)) {
+        matches.push(data.groups[group].matches.map((match) => transformMatch(match)))
+    }
+
+    response.send({ matches })
+}
+
+function transformMatch(match: any): Match {
+    return {
+        id: `${match.name}`,
+        homeTeamId: `${match.home_team}`,
+        awayTeamId: `${match.away_team}`,
+        status: MatchStatus.Pending,
+        date: new Date(match.date),
+        winnerTeamId: ''
+    }
 }
