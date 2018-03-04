b19ba60fe02eadcd6a68cb05fac061859e79a814
William Mora <william.r.mora@gmail.com>
2018-03-03 21:24:19 -0500

    Add jumbotron

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,24 +1,30 @@
 import React, { Component } from "react"
-import { Form, FormGroup, FormControl, ControlLabel, Button } from "react-bootstrap"
+import { Jumbotron, Form, FormGroup, FormControl, ControlLabel, Button } from "react-bootstrap"
 
 export default class LogIn extends Component {
   render() {
     return (
-      <Form>
-        <FormGroup>
-          <ControlLabel>Name</ControlLabel>
-          <FormControl type="text" placeholder="Jane Doe" />
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>Email</ControlLabel>
-          <FormControl type="email" placeholder="jane.doe@example.com" />
-        </FormGroup>
-        <FormGroup>
-          <ControlLabel>Password</ControlLabel>
-          <FormControl type="password" placeholder="Password" />
-        </FormGroup>
-        <Button type="submit">Send invitation</Button>
-      </Form>
+      <div>
+        <Jumbotron>
+          <h1>Welcome!</h1>
+          <p>Please log in to continue</p>
+        </Jumbotron>
+        <Form>
+          <FormGroup>
+            <ControlLabel>Name</ControlLabel>
+            <FormControl type="text" placeholder="Jane Doe" />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Email</ControlLabel>
+            <FormControl type="email" placeholder="jane.doe@example.com" />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Password</ControlLabel>
+            <FormControl type="password" placeholder="Password" />
+          </FormGroup>
+          <Button type="submit">Log in</Button>
+        </Form>
+      </div>
     )
   }
 }
